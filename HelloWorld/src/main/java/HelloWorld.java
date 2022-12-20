986589b2db9d53901abbeacbc2e1851ced640f04
jusufuhaines <jusufu.hainesjr@gmail.com>
2022-12-20 03:45:03 +0000

    Saved progress on MethodParameters 2022-12-20 03:45:03 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
      * You can use the test results to check for differences.
      * @return "Hello, world!"
      */
-    public static void main (String[] args){
+    public static void sayHello(String[] args){
         System.out.println("Hello, world!");
     }
     
