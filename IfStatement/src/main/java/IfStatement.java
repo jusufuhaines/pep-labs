1ac81b53df69301ce4675545baadcbf68563904d
jusufuhaines <jusufu.hainesjr@gmail.com>
2022-12-22 05:01:23 +0000

    Saved progress on IfStatement 2022-12-22 05:01:23 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -17,18 +17,15 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public static int ifExample(boolean bool, int x, int y){
-        if(bool=true){
+        if(bool == true){
             return x;
         }
         else{
-            bool = false;
             return y;
         }
     }
     public static void main(String args[]){
-        IfStatement try3 = new IfStatement();
-        IfStatement try1 = new IfStatement();
-        try3.ifExample(true, 0, 1);
-        try1.ifExample(false,0,1);
+        IfStatement.ifExample(true, 0,1);
+        IfStatement.ifExample(false, 0,1);
     }
 }
