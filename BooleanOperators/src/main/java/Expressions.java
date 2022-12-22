926c29076103ee5685fadd3935921e5426a78a1f
jusufuhaines <jusufu.hainesjr@gmail.com>
2022-12-22 08:30:49 +0000

    Saved progress on BooleanOperators 2022-12-22 08:30:49 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.TreeUI;
 
 /**
  * In order to make our programs even more impressive and interactive, we need to learn how to better leverage
@@ -13,8 +14,8 @@ public class Expressions {
      * @param b a true/false value.
      * @return if b is true, return true. if b is false, return false.
      */
-    public boolean returnBoolean(boolean b){
-        return false;
+    public static boolean returnBoolean(boolean b){
+        return b;
     }
 
     /**
@@ -22,8 +23,8 @@ public class Expressions {
      * @return if b is true, return false. if b is false, return true.
      * look into boolean operations to do this without using if/else statements.
      */
-    public boolean returnNot(boolean b){
-        return false;
+    public static boolean returnNot(boolean b){
+        return(!b);
     }
 
     /**
@@ -32,19 +33,31 @@ public class Expressions {
      * @return true if both b1 and b2 are true. false otherwise.
      * look into boolean operations to do this without using if/else statements.
      */
-    public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+    public static boolean returnAnd(boolean b1, boolean b2){
+         return(b1&b2);
     }
 
     /**
      * @param b1 a true/false value.
      * @param b2 a true/false value.
-     * @return true if either b1 or b1 are true. false if both are false.
+     * @return true if either b1 or b2 are true. false if both are false.
      * look into boolean operations to do this without using if/else statements.
      */
-    public boolean returnOr(boolean b1, boolean b2){
-        return false;
+    public static boolean returnOr(boolean b1, boolean b2){
+        return (b1|b2);
     }
+public static void main(String args[]){
+    Expressions.returnBoolean(true);
+    Expressions.returnBoolean(false);
 
+    Expressions.returnNot(true);
+    Expressions.returnNot(false);
+
+    Expressions.returnAnd(true, true);
+    Expressions.returnAnd(true, false);
+
+    Expressions.returnOr(true, false);
+    Expressions.returnOr(false,false);
+}
 
 }
