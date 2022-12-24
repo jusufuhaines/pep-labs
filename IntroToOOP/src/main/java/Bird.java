464ffaa2cd31e42a0839d062a316de6344368465
jusufuhaines <jusufu.hainesjr@gmail.com>
2022-12-24 08:31:48 +0000

    Saved progress on HelloWorld 2022-12-24 08:31:48 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -31,7 +31,7 @@ public class Bird {
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
-    public int ageMonths;
+    public static int ageMonths;
 
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
@@ -44,8 +44,14 @@ public class Bird {
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *  * greater than or equal to 3.
      */
-    public String sound(){
-        return "";
+    public static String sound(){
+        if(ageMonths<3){
+            return "Tweet";
+        }
+        else if(ageMonths>=3){
+            return "Chirp";
+        }
+        
     }
 
 }
