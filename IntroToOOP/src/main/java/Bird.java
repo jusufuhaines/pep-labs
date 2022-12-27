700bac2ee08974b61e9280d9c4b8b25985a9f11a
jusufuhaines <jusufu.hainesjr@gmail.com>
2022-12-27 02:57:45 +0000

    Saved progress on Instantiation 2022-12-27 02:57:45 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -31,7 +31,7 @@ public class Bird {
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
-   public static int ageMonths = 0;
+   public static int ageMonths;
 
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
