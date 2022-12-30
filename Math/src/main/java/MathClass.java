c841d92a57028efe225ad8f12cc012d9389710a5
jusufuhaines <jusufu.hainesjr@gmail.com>
2022-12-30 15:28:52 +0000

    Saved progress on Math 2022-12-30 15:28:52 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -19,7 +19,7 @@ public class MathClass {
      *
      * @return return a^b using the pow() method of the Math class.
      */
-    public double mathPow(double a, double b){
-        return 0;
+    public static double mathPow(double a, double b){
+        return Math.pow(a,b);
     }
 }
